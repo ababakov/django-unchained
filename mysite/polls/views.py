057632e07908e0d6d506f315b76936b99c139053
Anton Babakov <ababakov@inbox.ru>
2016-02-20 00:25:47 +0700

    went through tutorial
    fixed issue with no choices in question to show

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,26 +3,40 @@
 from django.http import  HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
-#from django.views import generic
+from django.views import generic
+from django.db.models import Count
+from django.utils import timezone
 
 from .models import Choice, Question
 
 # Create your views here.
 
-def index(request):
-	return Question.objects.order_by('-pub_date')[:5]
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+	def get_queryset(self):
+		"""
+		Return the last five published questions (not including those set to be
+		published in the future).
+		"""
+		return Question.objects.annotate(num_choices=Count('choice')).filter(
+			pub_date__lte=timezone.now(),
+			num_choices__gt=0
+		).order_by('-pub_date')[:5]
 
-	return render(request, 'polls/index.html', {
-		'latest_question_list' : latest_question_list
-	})
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+	def get_queryset(self):
+		"""
+		Excludes any questions that aren't published yet.
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question' : question})
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question' : question})
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
