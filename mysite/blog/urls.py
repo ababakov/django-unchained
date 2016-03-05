337ed2356d3238c777378acc5dc862004606058a
Anton Babakov <ababakov@inbox.ru>
2016-03-05 15:50:27 +0700

    fixed form layout; added url for form commit;

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,11 +1,14 @@
 # coding: utf-8
 
 from django.conf.urls import url
+from django.views.decorators.http import require_POST  # require_GET
 
-from blog.views import PostDetailView, PostsListView
+from blog.views import PostDetailView, PostsListView, CommentFormView
 
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', PostsListView.as_view(), name='post-list'),
-    url(r'^post/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post-detail')
+    url(r'^post/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post-detail'),
+    url(r'^post/comment/$', require_POST(CommentFormView.as_view()),
+        name='post-add-comment')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,10 @@
-from blog.models import Post
 from django.views.generic import ListView, DetailView, FormView
-from forms import NoUserCommentForm
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+
+from blog.models import Post
+from forms import NoUserCommentForm, UserCommentForm
 # , UserCommentForm
 
 
@@ -13,10 +17,26 @@ class PostDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(PostDetailView, self).get_context_data(**kwargs)
-        context['form'] = NoUserCommentForm
+        if self.request.user.is_authenticated():
+            context['form'] = UserCommentForm
+        else:
+            context['form'] = NoUserCommentForm
         return context
 
 
-class NoUserCommentFormView(FormView):
-    form_class = NoUserCommentForm
-    success_url = '/'
+class CommentFormView(FormView):
+    # form_class = UserCommentForm
+    # success_url = '/'
+    def get_form_class(self):
+        if self.request.user.is_authenticated():
+            return UserCommentForm
+        else:
+            return NoUserCommentForm
+
+    def get_success_url(self):
+        return reverse('blog:post-detail',
+                       args=(self.request.POST['post_id'],))
+
+    def form_valid(self, form):
+        # Post.objects.create(**form.cleaned_data)
+        return HttpResponseRedirect(self.get_success_url())
